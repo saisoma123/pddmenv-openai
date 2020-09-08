79d04fab835554e9b589376b024860eb501f436c
Surya Somayyajula <51333257+saisoma123@users.noreply.github.com>
2020-09-08 18:37:45 -0400

    Update __init__.py

--- robotgym/envs/__init__.py
+++ robotgym/envs/__init__.py
@@ -1,5 +1,4 @@
 from robotgym.envs.baoding_env import BaodingEnv
 from robotgym.envs.dclaw_env import DclawEnv
-from robotgym.envs.dhand_env import DhandEnv
 from robotgym.envs.cube_env import CubeEnv
-from robotgym.envs.pencil_env import PencilEnv
+
